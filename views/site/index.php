1e2e2c82bf298bd0423ed7978865e36e2d920188
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-06-30 13:37:20 +0330

    init project

---
+++ views/site/index.php
@@ -0,0 +1,26 @@
+<?php
+/* @var $this yii\web\View */
+
+$this->title = 'Beautiful Yii Application';
+?>
+
+<div class="site-index">
+    <div class="jumbotron text-center bg-primary text-white">
+        <h1>Deployed On Liara</h1>
+        <p class="lead">This app is deployed on Liara</p>
+    </div>
+
+    <div class="body-content">
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Liara Website</h2>
+                <p>for more informarion, you can check <a href="https://liara.ir">Liara</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Liara Github</h2>
+                <p>for more informarion, you can check <a href="https://github.com/liara-cloud">Github</a></p>
+            </div>
+            
+        </div>
+    </div>
+</div>
